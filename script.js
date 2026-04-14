b6f11638117b0b56188e9b958ff8690c027b9e23
iamaprokaryote <sdnevets8@gmail.com>
2026-04-14 20:12:34 +0000

    updated html, css, js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// add javascript here
+let namev1 = document.getElementById("name");
+const userName = namev1[0].toUpperCase() + namev1.slice(1);
+
+const getSelectedSize = () => {
+  const selected = document.querySelector('input[name="size"]:checked');
+  return selected ? selected.value : null;
+};
+
