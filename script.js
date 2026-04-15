af6c6ffbfc3b05ad3cc990daf4e7edd9ce50da39
iamaprokaryote <sdnevets8@gmail.com>
2026-04-15 00:57:13 +0000

    Day 2: I finalized my html layout (it will not change for now) and began working on my javascript functions. I assigned event listeners to all user input locations and finished the function to calculate total price.

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-const userName = document.getElementById('name');
+let username = document.getElementById('name').value;
+const formattedName = username ? username.charAt(0).toUpperCase() + username.slice(1).toLowerCase() : "";
 const sizeForm = document.getElementById('sizeForm');
 const toppingsForm = document.getElementById('toppings');
 const summary = document.getElementById('ordersummary');
 const submitBtn = document.getElementById('submitOrder');
+
+const prices = {
+    "Small": 10.00,
+    "Medium": 15.00,
+    "Large": 18.00,
+    "Xtra™ Large": 25.00
+};
+
+function calculateTotal(size, toppingCount) {
+    let basePrice = prices[size] || 0;
+    let extraCost = 0;
+
+    if (toppingCount > 2) {
+        extraCost = (toppingCount - 2) * 1.50;
+    }
+
+    return basePrice + extraCost;
+}
+
 
 
 
